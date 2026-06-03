0cbac54a1fc6ec6d8df9bc6a12c66c4321d1a2c3
Felipe Reiz Navas <skywalkerfelipe0@gmail.com>
2026-06-03 16:27:13 -0300

    Update Ilum.py

--- Ilum.py
+++ Ilum.py
@@ -1,1 +1,23 @@
+from flask import *
+app = Flask(__name__)
+@app.route("/", methods=["GET", "POST"])
+def ProcessResult():
+    Result = None
+    if request.method == "POST":
+        Num1 = float(request.form.get("1stVALUE"))
+        Num2 = float(request.form.get("2ndVALUE"))
+        Operation = request.form.get("OPERATION")
+        if Operation == "1":
+            Result = Num1 + Num2
+        elif Operation == "2":
+            Result = Num1 - Num2
+        elif Operation == "3":
+            Result = Num1 * Num2
+        elif Operation == "4":
+            Result = Num1 / Num2
+        else:
+            Result = "Invalid values!"
+    return render_template("calculator.html", Result=Result)
 
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
